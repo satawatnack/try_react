e7972f86ff9b629b0d48988660a324cf9c9e0fc7
satawatnack <s.nack1919@gmail.com>
2021-02-15 14:17:11 +0700

    add css scoped

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import './Layout.css';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>header</div>
-    <main className="Content">{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
--- my-app/src/containers/Home/Home.js
+++ my-app/src/containers/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Home.css';
+import classes from './Home.css';
 
 const Home = () => {
   return (
-    <div className="Content">
+    <div className={classes.Content}>
       <h3>Home</h3>
     </div>
   );
