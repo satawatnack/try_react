f0e9ed7a5a83a2ab0733a3d6b2e1f90f9f9254bf
satawatnack <s.nack1919@gmail.com>
2021-02-17 14:26:29 +0700

    add axios

--- my-app/src/containers/Launches/Launches.js
+++ my-app/src/containers/Launches/Launches.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import LaunchService from '../../services/LaunchService';
 
 const Launches = () => {
+  const [launchesData, setLaunchesData] = useState([]);
+
+  useEffect(() => {
+    getLaunches();
+  }, []);
+
+  const getLaunches = () => {
+    LaunchService.getAll(5, 5)
+      .then((res) => {
+        setLaunchesData(res.data);
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return (
     <div>
       <h3>Launches</h3>
---
+++ my-app/src/services/LaunchService.js
@@ -0,0 +1,14 @@
+import http from '../http-common';
+
+const getAll = (limit = 0, offset = 0) => {
+  return http.get(`launches?limit=${limit}&offset=${offset}`);
+};
+
+const get = (flight_number) => {
+  return http.get(`launches'/${flight_number}`);
+};
+
+export default {
+  getAll,
+  get,
+};
