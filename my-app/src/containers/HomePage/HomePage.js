f9445d1670a0c75a0c8ef5ec2bcbf7caf46f532c
Udomeak <60070120@it.kmitl.ac.th>
2021-02-21 18:56:58 +0700

    add HomePage

--- my-app/src/containers/HomePage/HomePage.js
+++ my-app/src/containers/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import Home from '../../components/Home/Home.js'
 import classes from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div className={classes.Content}>
-      <h3>Home</h3>
-    </div>
+    // <div className={classes.Content}>
+    //   <h3>Home</h3>
+    // </div>
+    <>
+      <Home />
+    </>
   );
 };
 
