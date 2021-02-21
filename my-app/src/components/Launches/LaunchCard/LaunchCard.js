d8e6cdb4fd60b964d222f769146cb86c042c16a0
satawatnack <s.nack1919@gmail.com>
2021-02-21 16:31:37 +0700

    add launch detail page

--- my-app/src/components/Launches/LaunchCard/LaunchCard.js
+++ my-app/src/components/Launches/LaunchCard/LaunchCard.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import classes from './LaunchCard.module.css';
+import { Link, useRouteMatch } from 'react-router-dom';
 
-const LaunchCard = (props) => (
-  <div
-    className={`${classes.LaunchCard} bg-gradient-to-r from-green-400 to-blue-500`}
-  >
-    <div className={classes.container}>
-      <h3>No : {props.launch?.flight_number ?? ''}</h3>
-      <h5>Mission : {props.launch?.mission_name ?? ''}</h5>
-      <ul>
-        <li>rocket name : {props.launch?.rocket?.rocket_name ?? ''}</li>
-        <li>
-          launch success:{' '}
-          <span
-            className={`${
-              props.launch?.launch_success ? classes.green : classes.red
-            }`}
-          >
-            {props.launch?.launch_success?.toString() ?? ''}
-          </span>
-        </li>
-        <li>launch year : {props.launch?.launch_year ?? ''}</li>
-      </ul>
+const LaunchCard = (props) => {
+  let { url } = useRouteMatch();
+  return (
+    <div
+      className={`${classes.LaunchCard} bg-gradient-to-r from-green-400 to-blue-500`}
+    >
+      <div className={classes.container}>
+        <h3>No : {props.launch?.flight_number ?? ''}</h3>
+        <h5>Mission : {props.launch?.mission_name ?? ''}</h5>
+        <ul>
+          <li>rocket name : {props.launch?.rocket?.rocket_name ?? ''}</li>
+          <li>
+            launch success:{' '}
+            <span
+              className={`${
+                props.launch?.launch_success ? classes.green : classes.red
+              }`}
+            >
+              {props.launch?.launch_success?.toString() ?? ''}
+            </span>
+          </li>
+          <li>launch year : {props.launch?.launch_year ?? ''}</li>
+          <li>
+            <Link
+              to={`${url}/${props.launch?.flight_number ?? ''}`}
+              className="navbar-item"
+            >
+              Click for detail
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LaunchCard;
