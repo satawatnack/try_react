53c61b0eabb17051626a9519409722180f5586df
satawatnack <s.nack1919@gmail.com>
2021-02-23 01:47:29 +0700

    add React Lazy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,22 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 
 import Home from './containers/HomePage/HomePage';
-import Rockets from './containers/RocketsPage/RocketsPage';
-import RocketDetail from './containers/RocketsPage/RocketDetail/RocketDetail';
-import Launches from './containers/LaunchesPage/LaunchesPage';
-import LaunchDetailPage from './containers/LaunchesPage/LaunchDetailPage/LaunchDetailPage';
+
+const Rockets = React.lazy(() =>
+  import('./containers/RocketsPage/RocketsPage')
+);
+
+const RocketDetail = React.lazy(() =>
+  import('./containers/RocketsPage/RocketDetail/RocketDetail')
+);
+
+const Launches = React.lazy(() =>
+  import('./containers/LaunchesPage/LaunchesPage')
+);
+
+const LaunchDetailPage = React.lazy(() =>
+  import('./containers/LaunchesPage/LaunchDetailPage/LaunchDetailPage')
+);
 
 import { LaunchProvider } from './contexts/LaunchContext';
 
@@ -15,19 +27,21 @@ function App() {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rockets" exact component={Rockets} />
-          <Route path="/rockets/:rocket_id" component={RocketDetail} />
+        <React.Suspense fallback={<span>Loading...</span>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rockets" exact component={Rockets} />
+            <Route path="/rockets/:rocket_id" component={RocketDetail} />
 
-          <LaunchProvider>
-            <Route path="/launches" exact component={Launches} />
-            <Route
-              path="/launches/:flightNumber"
-              component={LaunchDetailPage}
-            />
-          </LaunchProvider>
-        </Switch>
+            <LaunchProvider>
+              <Route path="/launches" exact component={Launches} />
+              <Route
+                path="/launches/:flightNumber"
+                component={LaunchDetailPage}
+              />
+            </LaunchProvider>
+          </Switch>
+        </React.Suspense>
       </Layout>
     </div>
   );
