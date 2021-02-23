3ca5f8123194d6e216fca9d537970e0297fac3df
satawatnack <s.nack1919@gmail.com>
2021-02-23 17:48:00 +0700

    edit success status

--- my-app/src/components/Launches/LaunchCard/LaunchCard.js
+++ my-app/src/components/Launches/LaunchCard/LaunchCard.js
@@ -4,6 +4,8 @@ import classes from './LaunchCard.module.css';
 
 const LaunchCard = (props) => {
   let { url } = useRouteMatch();
+  const launch_success = props.launch?.launch_success;
+
   return (
     <div
       className={`${classes.LaunchCard} bg-gradient-to-r from-gray-800 to-gray-600`}
@@ -20,15 +22,13 @@ const LaunchCard = (props) => {
               <li>Rocket : {props.launch?.rocket?.rocket_name ?? ''}</li>
               <li>
                 Success:{' '}
-                <span
-                  className={`${
-                    props.launch?.launch_success
-                      ? 'text-green-400'
-                      : 'text-red-400'
-                  }`}
-                >
-                  {props.launch?.launch_success?.toString() ?? ''}
-                </span>
+                {launch_success ? (
+                  <span className="text-green-400">success</span>
+                ) : launch_success === null ? (
+                  ''
+                ) : (
+                  <span className="text-red-400">unsuccess</span>
+                )}
               </li>
               <li>Year : {props.launch?.launch_year ?? ''}</li>
             </ul>
